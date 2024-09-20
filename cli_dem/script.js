cfd6d7354c29268e58ad94b7ca3f4f92857f3175
Divyansh Agrawal <divyanshagrawal@Divyanshs-MacBook-Air.local>
2024-09-20 15:28:52 +0530

    third

--- cli_dem/script.js
+++
@@ -1,9 +0,0 @@
-const args = process.argv;
-const num1 = parseInt(args[2]);
-const num2 = parseInt(args[3]);
-console.log(`The sum is: ${num1 + num2}`);
-
-console.log('args[0]:', args[0]);
-console.log('args[1]:', args[1]);
-console.log('args[2]:', args[2]);
-console.log('args[3]:', args[3]);
